01860b443bff0e7bbae99c2330c839a43d3cf386
Daniel Emerson <danielemerson@Daniels-MacBook-Pro-2.local>
2024-12-26 17:15:25 -0500

    first commit

---
+++ setup_venv.sh
@@ -0,0 +1,10 @@
+module load python/3.9.1
+python -m venv venv
+
+source ./venv/bin/activate
+
+pip install --upgrade pip
+pip install numpy
+pip install scipy
+pip install lib5c
+pip install matplotlib
